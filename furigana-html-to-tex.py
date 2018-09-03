f93893699b9e7877df0ada1fbd2f199ac1d1cfb0
Eliezer Cabrera Marin <eliezercabrera@gmail.com>
2018-09-02 23:44:30 -0500

    Clean up the main script
    Now that the main script doesn't use `mechanize` and is interpreted as
    a python 3 script, the HTML parser now makes use of Enums. Also, the
    parser logic is simpler now, thanks in large part to additional
    preprocessing of the lyrics that get rid of many edge cases.
    
    This commit deliberately doesn't add any new functionality.

--- furigana-html-to-tex.py
+++ furigana-html-to-tex.py
@@ -1,70 +1,48 @@
 from bs4 import BeautifulSoup
-from string import Template
 from collections import defaultdict
+from enum import Enum
+from html.parser import HTMLParser
+from string import Template
 
 import appex
-import requests
 import clipboard
-from html.parser import HTMLParser
+import requests
 import sys
 
 class MyHTMLParser(HTMLParser, object):
+	class Stage(Enum):
+		START = 1
+		DATA = 2
+		END = 3
+
 	def __init__(self):
 		super(MyHTMLParser, self).__init__()
 		self.NEXT_LINE = '\n\n'
 		self.NEXT_VERSE = '\n\n\\bigskip\n\n'
+
 		self.streak = ('', 0)
-		self.statuses = {
-			'is_active': {
-				'status': 'DORMANT',
-				'toggles': {
-					'body': {
-						'stage': 'AT_START',
-						'status': 'AWAKE'
-					},
-					'form': {
-						'stage': 'AT_START',
-						'status': 'DEAD'
-					}
-				}
-			}
-		}
-		self.awake_toggling_tags = {'body': True, 'form': False}
 		self.is_processing = defaultdict(lambda: False)
 		self.tex = []
 
 	def handle_starttag(self, tag, attrs):
-		self.refresh_statuses(tag, 'AT_START')
-
-		if self.statuses['is_active']['status'] is 'AWAKE':
-			self.is_processing[tag] = True
-			self.handle_streak(tag, 'AT_START') 
+		self.is_processing[tag] = True
+		self.handle_streak(tag, self.Stage.START) 
 
 	def handle_endtag(self, tag):
-		self.refresh_statuses(tag, 'AT_END')
-		
-		if self.statuses['is_active']['status'] == 'AWAKE':
-			self.is_processing[tag] = False
-			self.handle_streak(tag, 'AT_END')
+		self.is_processing[tag] = False
+		self.handle_streak(tag, self.Stage.END)
 
 	def handle_data(self, data):
-		if self.statuses['is_active']['status'] == 'AWAKE':
-			self.handle_streak('handle_data', 'AT_DATA')
-			
-			to_append = Template('$data')
-			if self.is_processing['rp']:
-				to_append = Template('')
-			if self.is_processing['rb']:
-				to_append = Template('{$data}')
-			if self.is_processing['rt']:
-				to_append = Template('{|$data}')
-			self.tex.append(to_append.substitute(data=data))
-
-	def refresh_statuses(self, tag, stage):
-		for key, value in self.statuses.items():
-			if tag in value['toggles']:
-				if value['toggles'][tag]['stage'] == stage:
-					self.statuses[key]['status'] = value['toggles'][tag]['status']
+		self.handle_streak('handle_data', self.Stage.DATA)
+		
+		to_append = Template('$data')
+		if self.is_processing['rp']:
+			to_append = Template('')
+		if self.is_processing['rb']:
+			to_append = Template('{$data}')
+		if self.is_processing['rt']:
+			to_append = Template('{|$data}')
+		self.tex.append(to_append.substitute(data=data))
 	
 	def reset_streak(self):
 		tag, _ = self.streak
@@ -80,13 +58,14 @@ class MyHTMLParser(HTMLParser, object):
 	def handle_streak(self, next_tag, stage):
 		to_append = ''
 		tag, count = self.streak
-		if tag == next_tag and stage == 'AT_START':
+		if tag == next_tag and stage == self.Stage.START:
 			self.increase_streak()
-		elif stage == 'AT_END' and tag == 'handle_data':
+		# TODO: Review if this elif is necessary.
+		elif stage == self.Stage.END and tag == 'handle_data':
 			self.reset_streak()
-		elif stage in ['AT_START', 'AT_DATA']:
+		elif stage in [self.Stage.START, self.Stage.DATA]:
 			if tag == 'ruby':
-				if stage == 'AT_START':
+				if stage == self.Stage.START:
 					to_append = '\\jruby'
 			if tag == 'br':
 				to_append = self.NEXT_LINE if count <= 2 else self.NEXT_VERSE				
@@ -94,14 +73,14 @@ class MyHTMLParser(HTMLParser, object):
 		self.tex.append(to_append)
 
 	def get_lyrics(self):
-		return ''.join(self.tex[:-3])
+		return ''.join(self.tex)
 
 def extract_song_info(url):
 	bs_raw_lyrics = BeautifulSoup(requests.get(url).text, "html5lib")
 	
-	artist = bs_raw_lyrics.find("div", class_="artistcontainer").get_text()
-	title = bs_raw_lyrics.find("div", class_="titlelyricblocknew").find("h1").get_text()
-	lyrics = bs_raw_lyrics.find("div", id="Lyrics").get_text()
+	artist = bs_raw_lyrics.find('div', class_='artistcontainer').get_text()
+	title = bs_raw_lyrics.find('div', class_='titlelyricblocknew').find('h1').get_text()
+	lyrics, _ = bs_raw_lyrics.find('div', id='Lyrics').get_text().split('eval(')
 	return (artist, title, lyrics)	
 
 def prepare_lyrics_for_furiganization(lyrics):
@@ -116,7 +95,17 @@ def prepare_lyrics_for_furiganization(lyrics):
 def furiganize_lyrics(lyrics):
 	url = 'http://furigana.sourceforge.net/cgi-bin/index.cgi'
 	data = {'text': (None, lyrics), 'state': (None, 'output')}
-	return requests.post(url, files=data).text
+	request = BeautifulSoup(requests.post(url, files=data).text, "html5lib")
+	body = str(request.find('body'))
+	
+	body_tag = '<body>'
+	start = body.find(body_tag)
+	
+	form_tag = '<form'
+	end = body.find(form_tag)
+
+	processed_lyrics = body[start + len(body_tag) : end]
+	return processed_lyrics
 
 def fill_tex_template(artist, title, lyrics):
 	template_file = open('tex_template.tex', 'r', encoding='utf-8')
@@ -136,9 +125,7 @@ def main():
 
 	parser = MyHTMLParser()
 	parser.feed(furigana_lyrics_html)
-	
-	print(parser.get_lyrics())
-	
+		
 	clipboard.set(fill_tex_template(artist, title, parser.get_lyrics()))
 
 if __name__ == '__main__':
